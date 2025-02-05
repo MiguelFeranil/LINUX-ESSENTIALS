a70a316035b132780921b82e03a6239751daaeca
MiguelFeranil <152839582+MiguelFeranil@users.noreply.github.com>
2025-02-05 01:45:03 +0000

    Activity 1

---
+++ activity1.sh
@@ -0,0 +1,12 @@
+echo "Hello, $USER_NAME! This is Activity 1 for OS LAB."
+
+echo "Enter your name:"
+read USER_NAME
+
+echo "Enter your age:"
+read USER_AGE
+
+echo "Enter your program:"
+read USER_PROGRAM
+
+echo "Hello, $USER_NAME. THIS IS YOUR $USER_AGE & YOUR PROGRAM IS $USER_PROGRAM."
---
+++ sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Declare a variable
+USER_NAME="Sudent"
+
+# Print a greeting using the variable
+echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+# Ask for user input
+echo "Enter your name:"
+read USER_NAME
+
+# Display a personalized message
+echo "Nice to meet you, $USER_NAME!"
